710b679753e299c01669a8b195be6607ef0aba07
Daniel Rubin <danielrubin95@hotmail.com>
2015-10-17 22:31:37 -0300

    trocando de php-framework-ts

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,14 +1,6 @@
 <?php
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::group([
-    'prefix' => 'teste'
-] , function(){
+Route::group(['prefix' => 'teste'] , function(){
     Route::get('cliente' , function(){
         return view('Client.Layout.default');
     });
