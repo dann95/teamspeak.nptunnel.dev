653d941a022651d4bb78262be1ff473e291480c9
Daniel Rubin <danielrubin95@hotmail.com>
2015-10-22 01:37:24 -0200

    ops, forgot the $planos

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -24,6 +24,7 @@ class FrontController extends Controller
      */
     public function index()
     {
+        $planos = $this->planRepository->actives();
         return view('Front.index' , compact('planos'));
     }
 }
