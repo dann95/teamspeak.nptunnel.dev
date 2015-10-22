bc9567203cb9504433ad465e913ac1390c8ff002
Daniel Rubin <danielrubin95@hotmail.com>
2015-10-22 06:01:00 -0200

    passing agent as default var on view

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,14 +7,16 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Auth\Guard;
+use Agent;
 
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-    public function __construct(Guard $auth)
+    public function __construct(Guard $auth , Agent $agent)
     {
         view()->share([
-            'auth'  =>  $auth
+            'auth'      =>  $auth,
+            'agent'     =>  $agent,
         ]);
     }
 }
