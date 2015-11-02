47202871af3fd63676585539750a98c43d3bd580
Daniel Rubin <danielrubin95@hotmail.com>
2015-11-02 16:53:41 -0200

    update models namespace..

--- app/Domain/Bot/Models/TsBot.php
+++ app/Domain/Bot/Models/TsBot.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace NpTS;
+namespace NpTS\Domain\Bot\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
--- app/Domain/HelpDesk/Models/QuestionCategory.php
+++ app/Domain/HelpDesk/Models/QuestionCategory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace NpTS;
+namespace NpTS\Domain\HelpDesk\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
