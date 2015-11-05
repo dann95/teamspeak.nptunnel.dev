1434a968423367cbaf4167c45ac42fb24767c836
Daniel Rubin <danielrubin95@hotmail.com>
2015-11-05 07:01:41 -0200

    wrong name for seeder class name..

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
 
         // $this->call(UserTableSeeder::class);
         $this->call(InvoiceStatusSeeder::class);
-        $this->call(QuestionCategory::class);
+        $this->call(QuestionCategorySeeder::class);
         Model::reguard();
     }
 }
