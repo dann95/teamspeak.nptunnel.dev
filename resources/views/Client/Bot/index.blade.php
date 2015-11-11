283bcc3fa6979715423aab3a8ba1be99d0f1be00
Daniel Rubin <danielrubin95@hotmail.com>
2015-11-11 08:52:45 -0200

    quick fix to view of bot.

--- resources/views/Client/Bot/index.blade.php
+++ resources/views/Client/Bot/index.blade.php
@@ -15,26 +15,37 @@
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
                     <li><a href="#">Ajustes</a></li>
+
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Player <span class="caret"></span></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Friend list <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="#">Listar</a></li>
-                            <li><a href="#">Adicionar Friend</a></li>
-                            <li><a href="#">Adicionar Enemy</a></li>
+                            <li class="dropdown-header">Players</li>
+                            <li><a href="#">Listar players</a></li>
+                            <li><a href="#">Adicionar player</a></li>
+                            <li><a href="#">Remover Player</a></li>
                             <li role="separator" class="divider"></li>
-                            <li class="dropdown-header">Nav header</li>
-                            <li><a href="#">Separated link</a></li>
-                            <li><a href="#">One more separated link</a></li>
+                            <li class="dropdown-header">Guilds</li>
+                            <li><a href="#">Listar guilds</a></li>
+                            <li><a href="#">Adicionar guild</a></li>
+                            <li><a href="#">Remover guild</a></li>
                         </ul>
                     </li>
+
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Guild <span class="caret"></span></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Enemy list <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="#">Listar</a></li>
-                            <li><a href="#">Adicionar Friend</a></li>
-                            <li><a href="#">Adicionar Enemy</a></li>
+                            <li class="dropdown-header">Players</li>
+                            <li><a href="#">Listar players</a></li>
+                            <li><a href="#">Adicionar player</a></li>
+                            <li><a href="#">Remover Player</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li class="dropdown-header">Guilds</li>
+                            <li><a href="#">Listar guilds</a></li>
+                            <li><a href="#">Adicionar guild</a></li>
+                            <li><a href="#">Remover guild</a></li>
                         </ul>
                     </li>
+
                 </ul>
             </div><!--/.nav-collapse -->
         </div><!--/.container-fluid -->
