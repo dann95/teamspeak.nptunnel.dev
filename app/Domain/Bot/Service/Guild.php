e223b82c7d09049c94f146d576139d28b9f2ae0a
Daniel Rubin <danielrubin95@hotmail.com>
2015-11-22 14:19:48 -0200

    add guild to database when added..

--- app/Domain/Bot/Service/Guild.php
+++ app/Domain/Bot/Service/Guild.php
@@ -7,6 +7,7 @@ use NpTS\Domain\Bot\Crawlers\Guild as GuildCrawler;
 use NpTS\Domain\Bot\Models\TibiaList;
 use NpTS\Domain\Bot\Service\Exceptions\CharacterAlreadyInThisList;
 use NpTS\Domain\Bot\Service\Exceptions\CharacterDosntExists;
+use NpTS\Domain\Bot\Service\Exceptions\GuildAlreadyInThisList;
 use NpTS\Domain\Bot\Service\Exceptions\GuildDosntExists;
 
 
@@ -26,10 +27,17 @@ class Guild
         if ($this->crawler->exists())
             throw new GuildDosntExists;
 
+        if (count($list->guilds()->where(['name' => $name])))
+            throw new GuildAlreadyInThisList;
+
         $chars = collect($this->crawler->characters());
 
         $chars->each(function($char) use($list , $position){
             Queue::push(new \NpTS\Domain\Bot\Jobs\AddCharacter($list , $char , $position));
         });
+        $list->guilds()->create([
+            'name' => $name,
+            'position' => $position
+        ]);
     }
 }
